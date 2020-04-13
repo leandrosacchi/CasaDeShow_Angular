6c0f5f710db27933579426b5d7b3c5583cdd181f
leandro-gft <59618736+leandro-gft@users.noreply.github.com>
2020-04-13 14:59:18 -0300

    Aula 9.14

--- casadeshowangular/src/app/app.module.ts
+++ casadeshowangular/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FuncionarioCardComponent
   ],
   imports: [
     BrowserModule,
