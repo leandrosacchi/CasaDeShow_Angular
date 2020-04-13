6fe9d8ee16d692232f57c902c3537e138e18a430
leandro-gft <59618736+leandro-gft@users.noreply.github.com>
2020-04-13 14:46:11 -0300

    Aula 9.15

--- casadeshowangular/src/app/app.component.ts
+++ casadeshowangular/src/app/app.component.ts
@@ -6,12 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  ultimoId = 0;
   nome = 'Leandro';
   adicionado = false;
+  funcionarios = [];
 
   adicionar() {
     console.log(`Adicionando ${this.nome}`)
     this.adicionado=true;
+
+    this.funcionarios.push({
+      id: ++this.ultimoId,
+      nome: this.nome
+    
+    });
   }
 
 }
