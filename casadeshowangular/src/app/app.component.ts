cceeae5145879b4af5a5e4aaa4f6575f8f874203
leandro-gft <59618736+leandro-gft@users.noreply.github.com>
2020-04-14 12:04:19 -0300

    Aula 9.15

--- casadeshowangular/src/app/app.component.ts
+++ casadeshowangular/src/app/app.component.ts
@@ -6,21 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  ultimoId = 0;
-  nome = 'Leandro';
-  adicionado = false;
+
   funcionarios = [];
 
-  adicionar() {
-    console.log(`Adicionando ${this.nome}`)
-    this.adicionado=true;
-
-    this.funcionarios.push({
-      id: ++this.ultimoId,
-      nome: this.nome
-    
-    });
+  aoAdicionar(funcionario){
+    this.funcionarios.push(funcionario);
   }
+  
 
 }
 
--- casadeshowangular/src/app/app.module.ts
+++ casadeshowangular/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FuncionarioCardComponent } from './funcionario-card/funcionario-card.component';
+import { FuncionarioFormComponent } from './funcionario-form/funcionario-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FuncionarioCardComponent
+    FuncionarioCardComponent,
+    FuncionarioFormComponent
   ],
   imports: [
     BrowserModule,
